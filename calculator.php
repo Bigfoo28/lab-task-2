452e282727a54d21fbbc12f9801ebf21a4675202
bigfoo28 <fouratia414@gmail.com>
2022-05-18 12:15:06 +0200

    calculator

--- calculator.php
+++ calculator.php
@@ -11,7 +11,7 @@ return $num1-$num2;
 {
 return $num1*$num2;
  }
- function divition(float $num1,float $num2): float
+ function division(float $num1,float $num2): float
 {
 return $num1/$num2;
  }
@@ -19,5 +19,39 @@ return $num1/$num2;
  echo addition(10,2), "<br/>" ;
  echo difference(10,2), "<br/>" ;
  echo multiplucation(10,2), "<br/>" ;
- echo divition(10,2) ,"<br/>" ;
+ echo division(10,2) ,"<br/>" ;
+ function calculator (float $num1,string $opperator,float $num2):float
+ { 
+     if(!( $opperator=='+'or $opperator=='-'or $opperator=='*'or $opperator=='/')){
+         return 0 ;
+     }
+     switch($opperator){
+         case '+':return addition($num1,$num2);
+         case '-':return difference($num1,$num2);
+         case '*':return multiplucation($num1,$num2);
+         case '/':return division($num1,$num2);
+     }
+ }
+ echo calculator(1,'+',2), "<br/>" ;
+ echo calculator(1,'-',2), "<br/>" ;
+ echo calculator(1,'*',2), "<br/>" ;
+ echo calculator(1,'/',2), "<br/>" ;
+ function calculatorv2 (float $num1,string $opperator,float $num2):float
+ { 
+     if(!in_array($opperator, ['+','-','*','/'])){
+         return 0 ;
+     }
+     $function_name=[
+         '+'=>'addition',
+         '-'=>'difference',
+         '*'=>'multiplucation',
+         '/'=>'division',
+     ];
+     return $function_name [$opperator]($num1,$num2); 
+    }
+
+ echo calculatorv2(1,'+',2);echo('addition+') , "<br/>" ;
+ echo calculatorv2(1,'-',2);echo('difference -'), "<br/>" ;
+ echo calculatorv2(1,'*',2);echo('multiplucation* '), "<br/>" ;
+ echo calculatorv2(1,'/',2);echo('division/'), "<br/>" ;
  
