81c35d8c05a70dac5d059ff286e35122928bda5a
bigfoo28 <fouratia414@gmail.com>
2022-05-18 11:17:56 +0200

    calculator

---
+++ calculator.php
@@ -0,0 +1,23 @@
+<?php
+function addition(float $num1,float $num2): float
+{
+return $num1+$num2;
+ }
+ function difference(float $num1,float $num2): float
+{
+return $num1-$num2;
+ }
+ function multiplucation(float $num1,float $num2): float
+{
+return $num1*$num2;
+ }
+ function divition(float $num1,float $num2): float
+{
+return $num1/$num2;
+ }
+
+ echo addition(10,2), "<br/>" ;
+ echo difference(10,2), "<br/>" ;
+ echo multiplucation(10,2), "<br/>" ;
+ echo divition(10,2) ,"<br/>" ;
+ 
