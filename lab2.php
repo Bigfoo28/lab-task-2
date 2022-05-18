ce904455bf520d07eee1181672e0a86c526ebdd0
bigfoo28 <fouratia414@gmail.com>
2022-05-18 11:00:14 +0200

    task5

--- lab2.php
+++ lab2.php
@@ -2,5 +2,18 @@
 function first(string $text): void 
 {
     echo ($text);
-}
+ }
 first('param3t3r');
+
+
+
+function repeatContent(int$count=1): void{
+ for ($i=0;$i<$count;$i++)
+ {
+    first("hi" );
+   
+   return;
+  }
+}
+repeatContent();
+repeatContent(5);
